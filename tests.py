b29bc4f02e7b94b57c5cc852d0b28e09b8c9fba3
Ivan Filimonov <snake73w@gmail.com>
2017-07-05 04:40:00 +0700

    update requirements

--- tests.py
+++ tests.py
@@ -1,7 +1,8 @@
 import pytest
+
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 
